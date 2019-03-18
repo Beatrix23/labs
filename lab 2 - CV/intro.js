07d4368b58653b90ec266ba17a842007c3a579c0
beatrixs <beatrixsocaci@gmail.com>
2019-03-18 19:40:58 +0200

    schimbari lab 6

---
+++ lab 2 - CV/intro.js
@@ -0,0 +1,60 @@
+function getWelcomeMsg() {
+    return "Wellcome to my page!";
+}
+
+
+function getColor() {
+    var date = new Date ();
+    console.log(date.getMinutes());
+    if(date.getMinutes() > 18) {
+         return "green"
+    }
+    return "red";
+
+}
+
+var wellcome = getWelcomeMsg();
+console.info(wellcome);
+
+var summaryElement = document.getElementById("summary");
+console.info("summaryElement", summaryElement);
+var color = getColor();
+summaryElement.style.color = color;
+
+function hidePage(page) {
+    var el = document.getElementById(page);
+    el.style,display = 'none' ;
+}
+ 
+function showPage(page) {
+    document.getElementById(page).style.display = 'block';
+}
+
+function initMenu() {
+    var links = document.querySelectorAll("#top-menu-bar a");
+    console.info(links);
+    for(var i = 0; i < links.length; i++) {
+        links[i].onclick = clickOnMenuItem;
+
+    }
+}
+
+function clickOnMenuItem () {
+    console.warn('click on menu', this);
+    hideAllPages();
+    var pageId = this.getAttribute('data-page');
+    console.warn({pageId});
+    showPage(pageId);
+
+}
+
+function hideAllPages() {
+    var pages = document.querySelectorAll(".page");
+    for (var i = 0; i < pages.length; i++ ) {
+        pages[i].style.display = 'none';
+    }
+}
+
+initMenu();
+
+
